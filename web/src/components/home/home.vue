7028b641e075f57dd42276e3c9aa51307bac8945
Pyingde <626935356@qq.com>
2017-10-28 16:10:08 +0800

    vv

--- web/src/components/home/home.vue
+++ web/src/components/home/home.vue
@@ -3,15 +3,16 @@
 		<div class="toolbar">
 			<div class="home_header">xx餐厅 12号桌</div>
 			<ul class="home_body">
-				<li><router-link to="/floor1">推荐菜</router-link></li>
-				<li><router-link to="/floor1">会员</router-link></li>
-				<li><router-link to="/floor1">点餐</router-link></li>
-				<li><router-link to="/floor1">呼叫服务</router-link></li>
-				<li><router-link to="/floor1">已点菜</router-link></li>
-				<li><router-link to="/floor1">购物车</router-link></li>
+				<li><i class="iconfont icon-menguo"></i><router-link to="/floor1">推荐菜</router-link></li>
+				<li><i class="iconfont icon-huiyuan"></i><router-link to="/floor1">会员</router-link></li>
+				<li><i class="iconfont icon-diancanguanli"></i><router-link to="/floor1">点餐</router-link></li>
+				<li><i class="iconfont icon-hujiao"></i><router-link to="/floor1">呼叫服务</router-link></li>
+				<li><i class="iconfont icon-diancan"></i><router-link to="/floor1">已点菜</router-link></li>
+				<li><i class="iconfont icon-gouwuche"></i><router-link to="/floor1">购物车</router-link></li>
 			</ul>
 			<div class="home_footer"></div>
 		</div>
+		
 		<div>
 			<router-view></router-view>
 		</div>
