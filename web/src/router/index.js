e16bb7a43a2e546d57a9aade0297df1eeec9251a
tsg <350298719@qq.com>
2017-10-28 10:29:42 +0800

    test

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -2,8 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeComponent from '../components/home/home.vue'
 import GoodsListComponent from '../components/goodslist/goodslist.vue'
-import MenuComponent from '../components/menu/menu.vue'
-import Order_pageComponent from '../components/order_page/order_page.vue'
+import Menu from '../components/menu/menu.vue'
+import Order_page from '../components/order_page/order_page.vue'
 
 Vue.use(VueRouter)
 
