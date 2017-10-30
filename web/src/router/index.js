817be248825a3ee3e0ea7019750dcdd5a3a7b2a6
tsg <350298719@qq.com>
2017-10-30 17:27:47 +0800

    qq

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
 import HomeComponent from '../components/home/home.vue'
 import GoodsListComponent from '../components/goodslist/goodslist.vue'
-import MenuComponent from '../components/menu/menu.vue'
+
 import Order_pageComponent from '../components/order_page/order_page.vue'
+import cookComponent from '../components/cook/cook.vue'
+
 
 // csx
 import Menu_header from '../components/menu_header/menu_header.vue'
 import Menu_left from '../components/menu_left/menu_left.vue'
 import Menu_datagrid from '../components/menu_datagrid/menu_datagrid.vue'
+import Menu_footer from '../components/menu_footer/menu_footer.vue'
 
 Vue.use(VueRouter)
 
@@ -19,6 +21,11 @@ var router = new VueRouter({
 			path: '/',
 			name: 'home',
 			component: HomeComponent,
+			children: [{
+				path: 'menu',
+				name: 'menu',
+				component: MenuComponent
+			}]
 			// children: [{
 			// 	path: 'menu',
 			// 	name: 'menu',
@@ -32,13 +39,20 @@ var router = new VueRouter({
 				// default: MenuComponent,
 				header: Menu_header,
 				left: Menu_left,
-				datagrid: Menu_datagrid
+				datagrid: Menu_datagrid,
+				footer: Menu_footer
 			}
+
 		},
 		{
 			path: '/order_page',
 			name: 'order_page',
 			component: Order_pageComponent
+		},
+		{
+			path: '/cook',
+			name: 'cook',
+			component: cookComponent
 		}
 	]
 })
