60ba11b1d3487fc2be3ac4c402d25815b94bd57d
Pyingde <626935356@qq.com>
2017-11-02 14:28:09 +0800

    sa

--- web/src/components/menu_footer/menu_footer.vue
+++ web/src/components/menu_footer/menu_footer.vue
@@ -79,8 +79,6 @@
                 var number = [];
                 var allprice = [];
                 var url = [];
-
-                console.log($('.foodlist_ul li'))
                 $('.foodlist_ul li').map(function(idx,item){
 
                     name.push($(item).find('h2').text().match(/\D*/));
@@ -88,46 +86,27 @@
                     number.push($(item).find('.dex span b').text());
                     allprice.push($(item).find('.dex p').text());
                     url.push($(item).find('.img img').attr('src'));
+                    
+                    
 
-                })
-                
+               })
                 http.post({
                     url: "menuadd", vm: this, params: {'idx': idx, 'name': name, 'price': price , 'number': number, 'allprice': allprice, 'url': url }}).then(res => {
                     self.datagrid = res.data;
                     console.log(res.data)
                 })
-                    // 
-                // var arrs = [];
-                // console.log(66)
-                // for(var i=0; i<$('.foodlist ul li').length; i++){
-                //     var idx = $('.foodlist ul li').eq(i).find('h2 span').html();
-                //     this.idx.push(idx);
-                // }
-                // socket.emit('menu_cd',this.idx);
+				var order_obj = {'idx': idx, 'name': name, 'price': price , 'number': number, 'allprice': allprice, 'url': url }
+				 console.log(order_obj);
+
+               
+                socket.emit('menu_cd',order_obj);
                 
-                // 存入购物车
-                // console.log(this.$parent.$el)
-                // console.log(this.$parent.$children[2].idx)
+               
                 
             }
         },
         mounted: function(){
-            // var self = this;
-            // http.post({
-            //     url: "select"
-            // ,vm:this}).then(res => {
-            //     self.datagrid = res.data;
-            //     console.log(res.data)
-            // })
-
-            // 点击加入菜单重新价格计算addnum
-                // var foot_money = 0;
-                // for(var i=0; i<$('.foodlist_ul li').length ; i++){
-                //     var foodlist_money = $('.foodlist_ul li').eq(i);
-                //     foodlist_money.find('.dex p').html(foodlist_money.find('.dex span').eq(0).html()*foodlist_money.find('.dex span').eq(1).find('b').html())
-                //     foot_money += Number(foodlist_money.find('.dex p').html());
-                // }
-                // $('#menu-footer .money p').html('￥'+foot_money)
+            
 
             
         }
